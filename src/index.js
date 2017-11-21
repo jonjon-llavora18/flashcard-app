8be9fbadb1f91ebe29ad195f3c74a2ccfaf4b0c1
Argel <timothy.llavora@bibo.com.ph>
2017-11-21 15:11:50 +0800

    used action creators

--- src/index.js
+++ src/index.js
@@ -56,16 +56,32 @@ const App = ({children}) => {
 }
 
 class Sidebar extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    const el = ReactDOM.findDOMNode(this.refs.add);
+    if(el) el.focus()
+   }
+
+ createDeck(evt) {
+  if(evt.which !== 13) return;
+
+  const name = ReactDOM.findDOMNode(this.refs.add).value;
+  this.props.addDeck(name);
+  this.props.hideAddDeck();
+ }
+
   render() {
     const props = this.props;
 
     return (
       <div className="sidebar">
         <h2>All Decks</h2>
+        <button onClick={props.showAddDeck}>
+          + New Deck
+        </button>
         <ul>
           {props.decks.map((deck, i) => <li key={i}>{deck.name}</li>)}
         </ul>
-        {props.addingDeck && <input ref="add" />}
+        {props.addingDeck && <input ref="add" onKeyPress={this.createDeck.bind(this)} />}
       </div>
     )
   }
