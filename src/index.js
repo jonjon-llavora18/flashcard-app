7fbf390102ed51d20809703ddfb1bcc2a6120081
Argel <timothy.llavora@bibo.com.ph>
2017-11-21 14:42:25 +0800

    create sidebar component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, combineReducers} from 'redux';
 import * as c from './constants';
@@ -30,7 +30,27 @@ const App = ({children}) => {
   )
 }
 
+class Sidebar extends Component {
+  render() {
+    const props = this.props;
+
+    return (
+      <div className="sidebar">
+        <h2>All Decks</h2>
+        <ul>
+          {props.decks.map((deck, i) => <li key={i}>{deck.name}</li>)}
+        </ul>
+        {props.addingDeck && <input ref="add" />}
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <App>Hello <strong>React</strong></App>,
+  <App>
+    <Sidebar
+      decks={[{name: 'deck 1'}, {name: 'deck 2'}, {name: 'deck 3'}]}
+      addingDeck={true} />
+  </App>,
   document.getElementById('root')
 );
