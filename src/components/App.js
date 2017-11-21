4cb52a3af04f2de050870f2141d52d447c144f75
Argel <timothy.llavora@bibo.com.ph>
2017-11-21 18:47:20 +0800

    created toolbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Sidebar from './Sidebar';
+import Toolbar from './Toolbar';
 
 const mapState = (props, {params: {deckId}}) => ({
   deckId
@@ -9,8 +10,8 @@ const mapState = (props, {params: {deckId}}) => ({
 const App = ({children, deckId}) => {
   return (
     <div className="app">
+    	<Toolbar deckId={deckId} />
       <Sidebar />
-      <h2>Deck: {deckId}</h2>
       {children}
     </div>
   )
